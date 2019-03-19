43fd1a0e9abce3c15fa3306b3f3fed5d00e26c20
harunsaybak <harun_saybak@hotmail.com>
2019-03-19 15:07:30 +0300

    velocity yerine position kullanildi

--- test_flight.py
+++ test_flight.py
@@ -1,166 +1,88 @@
 #!/usr/bin/env python
-# vim:set ts=4 sw=4 et:
-#
-# Copyright 2015 UAVenture AG.
-#
-# This program is free software; you can redistribute it and/or modify
-# it under the terms of the GNU General Public License as published by
-# the Free Software Foundation; either version 3 of the License, or
-# (at your option) any later version.
-#
-# This program is distributed in the hope that it will be useful, but
-# WITHOUT ANY WARRANTY; without even the implied warranty of MERCHANTABILITY
-# or FITNESS FOR A PARTICULAR PURPOSE. See the GNU General Public License
-# for more details.
-#
-# You should have received a copy of the GNU General Public License along
-# with this program; if not, write to the Free Software Foundation, Inc.,
-# 59 Temple Place, Suite 330, Boston, MA 02111-1307 USA
-# Updated: Tarek Taha : tarek.taha@kustar.ac.ae, Vladimir Ermakov
-#    - Changed topic names after re-factoring : https://github.com/mavlink/mavros/issues/233
-#    - Use mavros.setpoint module for topics
 
-import rospy
-import thread
+import rospy 
+#from threading import Thread, Timer
+#import time
 import threading
 import time
+import math
+#from geometry_msgs.msg import Twist
 import mavros
-
-from math import *
-from mavros.utils import *
-#from mavros import setpoint as SP
-#from tf.transformations import quaternion_from_euler
 from mavros_msgs.msg import PositionTarget
 from mavros_msgs.srv import *
 from std_msgs.msg import String
-from mavros_msgs.srv import CommandBool, SetMode
-
-class SetpointPosition:
-    """
-    This class sends position targets to FCU's position controller
-    """
-    def __init__(self):
-        self.x = 0.0
-        self.y = 0.0
-        self.z = 0.0
-        self.yaw= 0.0
-        # publisher for mavros/setpoint_position/local
-        self.pub = rospy.Publisher('/mavros/setpoint_raw/local', PositionTarget,queue_size=10)
-        #self.pub = SP.get_pub_position_local(queue_size=10)
-        # subscriber for mavros/local_position/local
-        #self.sub = rospy.Subscriber('yaw_degrees', String, self.reached)
-        #self.sub = rospy.Subscriber(mavros.get_topic('local_position', 'pose'),
-        #                            SP.PoseStamped, self.reached)
-
-        try:
-            thread.start_new_thread(self.navigate, ())
-        except:
-            fault("Error: Unable to start thread")
-
-        # TODO(simon): Clean this up.
-        self.done = False
-        self.done_evt = threading.Event()
-    """
-    def reached(self,data):
-        
-        yaw_ =data.data
-
-        if yaw_ > self.yaw -10 and yaw_ < self.yaw +10: 
-            self.done = True
-            #self.done_evt.set()
-"""
-
-    def navigate(self):
-        rate = rospy.Rate(10)   # 10hz
-
-        msg = PositionTarget()
-        msg.header.stamp = rospy.Time.now()
-        msg.header.frame_id= "world"
-        msg.coordinate_frame = 8
-        msg.type_mask=2503
-        """
-        msg = SP.PoseStamped(
-            header=SP.Header(
-                frame_id="base_footprint",  # no matter, plugin don't use TF
-                stamp=rospy.Time.now()),    # stamp should update
-        )"""
-
-        while not rospy.is_shutdown():
-            msg.velocity.x = self.x
-            msg.velocity.y = self.y
-            msg.velocity.z = self.z
-            msg.yaw = self.yaw
-            # For demo purposes we will lock yaw/heading to north.
-            
-            self.pub.publish(msg)
-            rate.sleep()
-
-    def set(self, x, y, z,yaw, delay=0, wait=True):
-        self.done = False
-        self.x = x
-        self.y = y
-        self.z = z
-        self.yaw = (yaw*6.3)/360
-
-        """     
-        if wait:
-            rate = rospy.Rate(5)
-            while not self.done and not rospy.is_shutdown():
-                rate.sleep()
-        """
-        time.sleep(delay)
-
-    
-
-def setpoint_demo():
-    rospy.init_node('talker',anonymous=True)
-    #mavros.set_namespace()  # initialize mavros module with default namespace
-    rate = rospy.Rate(10)
-
-    setpoint = SetpointPosition()
-    
-    setpoint.set(0.0, 0.0, 0.0, 0.0, 5.0)  # offboard modundan once veri gonderilir, 5 saniye bekle
-    #setOffboardMode
-    set_mode_client = rospy.ServiceProxy('/mavros/set_mode', SetMode) 
-    set_mode_client(base_mode=0, custom_mode="OFFBOARD")
-   
-    rospy.loginfo("Climb")
-    setpoint.set(0.0, 0.0, 2.0, 0.0, 3.0)
-    
-
-    rospy.loginfo("yaw")
-    setpoint.set(0.0, 0.0, 0.0, -90.0,6.0)
-    
-    rospy.loginfo("Fly to the forward")
-    setpoint.set(0.0, 2.0, 0.0, -90.0,3)
-
-    rospy.loginfo("yaw 180")
-    setpoint.set(0.0, 0.0, 0.0, 90.0,6.0)
-
-    rospy.loginfo("Fly to the forward")
-    setpoint.set(0.0, 2.0, 0.0, 90.0,3.0)
 
 
-   
+def _state_callback(topic):
 
-if __name__ == '__main__':
+	print "Mode: %s" % topic.mode
+	
+
+if  __name__ == '__main__':
+
+	rospy.init_node('talker',anonymous=True)
+	rate = rospy.Rate(20)
+	mavros.set_namespace('mavros')
+
+	state_sub = rospy.Subscriber(mavros.get_topic('state'), mavros_msgs.msg.State, _state_callback)
+
+	rospy.wait_for_service('/mavros/cmd/arming')
+	 	
+	try:
+		armService = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
+		armService(True)
+	except rospy.ServiceException, e:
+	 	pass
+	
+	time.sleep(3)
+
+	
 
 
-   
+	msg = PositionTarget()
 
-    rospy.wait_for_service('/mavros/cmd/arming')
-         
-    try:
-        armService = rospy.ServiceProxy('/mavros/cmd/arming', mavros_msgs.srv.CommandBool)
-        armService(True)
+	msg.header.stamp = rospy.Time.now()
+	msg.header.frame_id = "world"
+	msg.coordinate_frame = PositionTarget.FRAME_BODY_NED
+	msg.type_mask = PositionTarget.IGNORE_VX | PositionTarget.IGNORE_VY | PositionTarget.IGNORE_VZ | PositionTarget.IGNORE_AFX | PositionTarget.IGNORE_AFY | PositionTarget.IGNORE_AFZ | PositionTarget.IGNORE_YAW | PositionTarget.IGNORE_YAW_RATE
+
+	msg.position.x = 0.0
+	msg.position.y = 0.0
+	msg.position.z = 2.0
+
+	pub = rospy.Publisher('/mavros/setpoint_raw/local', PositionTarget, queue_size=10)
+
+	for i in range(0,50):
+		pub.publish(msg)
+		rate.sleep()
 
 
+	set_mode = rospy.ServiceProxy(mavros.get_topic('set_mode'), mavros_msgs.srv.SetMode)
+	set_mode(0,'OFFBOARD')
 
-    except rospy.ServiceException, e:
-        pass
+	
 
-    print "started"
-    try:
-        setpoint_demo()
-    except rospy.ROSInterruptException:
-        pass
+	counter = 0
+
+	try:
+
+		while not rospy.is_shutdown():
+			pub.publish(msg)
+
+			counter= counter + 1
+
+			if( counter>=50):
+
+				msg.type_mask ^= PositionTarget.IGNORE_YAW
+				msg.yaw = math.pi
+				print counter
+
+			if( counter>=100):
+
+				set_mode(0,'AUTO.LAND')
+
+			rate.sleep()
+
+	except rospy.ROSInterruptException:
+		pass
+
